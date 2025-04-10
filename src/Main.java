b8182cac83fa10853c2a9cc62fa107f457551150
mychemicalcode <jty7027@gmail.com>
2025-04-10 18:57:16 +0900

    6th commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //
+        //
         Menu menu = new Menu ();
         Kiosk kiosk = new Kiosk ();
         MenuItem metnuItem = new MenuItem();
