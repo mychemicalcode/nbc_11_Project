f7712e47e6b27845493146d09db24b0ce5e1e2bb
mychemicalcode <jty7027@gmail.com>
2025-04-10 18:49:20 +0900

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +7,8 @@ public class Main {
         MenuItem metnuItem = new MenuItem();
         Cart cart = new Cart();
         UserType userType = new UserType();
-
+        // develop commit 확인용
+        // main-class-basic => feature/lasttest
 
 
 
