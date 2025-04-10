345dec9a085dff2e27f7b08f2830eb7b5062e269
김진환 <jake.k9245@gmail.com>
2025-04-10 20:43:40 +0900

    first commit

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.awt.*;
+
+// 데이터 저장소 Menu데이터를 가져오는 class ??
 
 public class Kiosk {
+    private List<Menu> menus;
+
+    public Kiosk(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public List<Menu> getMenus() {
+        return manus;
+    }
+
 }
--- src/KioskService.java
+++ src/KioskService.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class KioskService {
+    private Scanner sc;
+    private Kiosk kiosk;
+    private CartService cartService;
+
+    public KioskService(Scanner sc, Kiosk kiosk, CartService cartService) {
+        this.sc = sc;
+        this.kiosk = kiosk;
+        this.cartService = cartService;
+    }
+
+    public void start() {
+        System.out.println("키오스크에 오신 것을 환영합니다.");
+
+
+    }
+
 }
