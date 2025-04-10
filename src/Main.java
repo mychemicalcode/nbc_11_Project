b6468f5dcc8e0caae6ca93b582c657c58fcddb78
mychemicalcode <jty7027@gmail.com>
2025-04-10 18:51:55 +0900

    5th commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-
-
 public class Main {
     public static void main(String[] args) {
+        //
         Menu menu = new Menu ();
         Kiosk kiosk = new Kiosk ();
         MenuItem metnuItem = new MenuItem();
         Cart cart = new Cart();
         UserType userType = new UserType();
-        // develop commit 확인용
-        // main-class-basic => feature/lasttest
+
 
 
 
