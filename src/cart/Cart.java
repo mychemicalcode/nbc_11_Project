fd5241d2c6a8c69977b4496e2a6e76cc5ca1c4c8
jaemin <bohemian1864@gmail.com>
2025-04-11 14:45:09 +0900

    수정

---
+++ src/cart/Cart.java
@@ -0,0 +1,14 @@
+package cart;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+
+    private final Map<MenuItem, Integer> cartItems = new HashMap<>();
+
+    public Map<MenuItem, Integer> getCartItems() {
+        return cartItems;
+    }
+}
