59c65a355a9396b5b7a66f6ccd250aa463d7c984
ghkdrudals75 <ghkdrudals75@naver.com>
2025-04-11 14:51:09 +0900

    수정

---
+++ src/Menu/Menu.java
@@ -0,0 +1,22 @@
+package Menu;
+
+import java.util.List;
+
+public class Menu {
+
+    private final String name;
+    private final List<MenuItem> menuItems;
+
+    public Menu(String name, List<MenuItem> menuItems) {
+        this.name = name;
+        this.menuItems = menuItems;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
---
+++ src/Menu/MenuItem.java
@@ -0,0 +1,26 @@
+package Menu;
+
+public class MenuItem {
+
+    private final String name;
+    private final double price;
+    private final String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
