8d547af82195b1505a67da4d00fcc79b0312c0cd
mychemicalcode <jty7027@gmail.com>
2025-04-11 15:01:32 +0900

    250411 15:00 commit_백업

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //
-        //
-        Menu menu = new Menu ();
-        Kiosk kiosk = new Kiosk ();
-        MenuItem metnuItem = new MenuItem();
-        Cart cart = new Cart();
-        UserType userType = new UserType();
 
-
+        Menu burgers = new Menu("Burgers",
+                new ArrayList<>(
+                        List.of(
+                                new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
+                                new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
+                                new MenuItem("Cheeseburger", )
+                        )
+                )
+                )
 
 
 
--- src/UserType.java
+++ src/UserType.java
@@ -1,2 +1,19 @@
 public class UserType {
+
+    private String name;
+    private int price;
+    private String description;
+
+    public MenuItem (String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {return this.name;}
+
+    public int getPrice() {return this.price;}
+
+    public String getDescription() {return this.description;}
+
 }
